b0625996610b153ca7ca1762ec4c77a05267943f
DevExpressExampleBot <fakeEmail>
2019-05-20 17:08:44 +0300

    Source auto update [skip ci]

--- VB/Q273845/Form1.Designer.vb
+++ VB/Q273845/Form1.Designer.vb
@@ -1,148 +1,93 @@
-﻿Namespace Q273845
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace Q273845
+    Partial Class Form1
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
 
-		#Region "Windows Form Designer generated code"
+            MyBase.Dispose(disposing)
+        End Sub
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.productsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.colProductName = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colUnitsInStock = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colUnitPrice = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colPrice = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colCategory = New DevExpress.XtraGrid.Columns.GridColumn()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.productsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.DataSource = Me.productsBindingSource
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.Size = New System.Drawing.Size(386, 538)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' productsBindingSource
-			' 
-			Me.productsBindingSource.DataMember = "Products"
-			' 
-			' gridView1
-			' 
-			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colProductName, Me.colUnitsInStock, Me.colUnitPrice, Me.colPrice, Me.colCategory})
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.GroupCount = 1
-			Me.gridView1.GroupSummary.AddRange(New DevExpress.XtraGrid.GridSummaryItem() {
-				New DevExpress.XtraGrid.GridGroupSummaryItem(DevExpress.Data.SummaryItemType.Sum, "UnitPrice", Me.colUnitPrice, "", "UnitPrice"),
-				New DevExpress.XtraGrid.GridGroupSummaryItem(DevExpress.Data.SummaryItemType.Sum, "UnitsInStock", Me.colUnitsInStock, "", "UnitsInStock"),
-				New DevExpress.XtraGrid.GridGroupSummaryItem(DevExpress.Data.SummaryItemType.Custom, "", Me.colPrice, "", "Price")
-			})
-			Me.gridView1.Name = "gridView1"
-			Me.gridView1.OptionsView.ShowFooter = True
-			Me.gridView1.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() { New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.colCategory, DevExpress.Data.ColumnSortOrder.Ascending)})
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.gridView1.CustomUnboundColumnData += new DevExpress.XtraGrid.Views.Base.CustomColumnDataEventHandler(this.gridView1_CustomUnboundColumnData);
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.gridView1.CustomDrawFooterCell += new DevExpress.XtraGrid.Views.Grid.FooterCellCustomDrawEventHandler(this.gridView1_CustomDrawFooterCell);
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.gridView1.CustomDrawRowFooterCell += new DevExpress.XtraGrid.Views.Grid.FooterCellCustomDrawEventHandler(this.gridView1_CustomDrawRowFooterCell);
-			' 
-			' colProductName
-			' 
-			Me.colProductName.Caption = "Product Name"
-			Me.colProductName.FieldName = "ProductName"
-			Me.colProductName.Name = "colProductName"
-			Me.colProductName.Visible = True
-			Me.colProductName.VisibleIndex = 0
-			' 
-			' colUnitsInStock
-			' 
-			Me.colUnitsInStock.Caption = "Units In Stock"
-			Me.colUnitsInStock.FieldName = "UnitsInStock"
-			Me.colUnitsInStock.Name = "colUnitsInStock"
-			Me.colUnitsInStock.SummaryItem.SummaryType = DevExpress.Data.SummaryItemType.Sum
-			Me.colUnitsInStock.Visible = True
-			Me.colUnitsInStock.VisibleIndex = 1
-			' 
-			' colUnitPrice
-			' 
-			Me.colUnitPrice.Caption = "UnitPrice"
-			Me.colUnitPrice.FieldName = "UnitPrice"
-			Me.colUnitPrice.Name = "colUnitPrice"
-			Me.colUnitPrice.SummaryItem.SummaryType = DevExpress.Data.SummaryItemType.Sum
-			Me.colUnitPrice.Visible = True
-			Me.colUnitPrice.VisibleIndex = 2
-			' 
-			' colPrice
-			' 
-			Me.colPrice.Caption = "Price"
-			Me.colPrice.FieldName = "Price"
-			Me.colPrice.Name = "colPrice"
-			Me.colPrice.SummaryItem.SummaryType = DevExpress.Data.SummaryItemType.Custom
-			Me.colPrice.UnboundType = DevExpress.Data.UnboundColumnType.Decimal
-			Me.colPrice.Visible = True
-			Me.colPrice.VisibleIndex = 3
-			' 
-			' colCategory
-			' 
-			Me.colCategory.Caption = "Category"
-			Me.colCategory.FieldName = "Category"
-			Me.colCategory.Name = "colCategory"
-			Me.colCategory.Visible = True
-			Me.colCategory.VisibleIndex = 4
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(386, 538)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.productsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.productsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.colProductName = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colUnitsInStock = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colUnitPrice = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colPrice = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colCategory = New DevExpress.XtraGrid.Columns.GridColumn()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.productsBindingSource), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            Me.gridControl1.DataSource = Me.productsBindingSource
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(386, 538)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
+            Me.productsBindingSource.DataMember = "Products"
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colProductName, Me.colUnitsInStock, Me.colUnitPrice, Me.colPrice, Me.colCategory})
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.GroupCount = 1
+            Me.gridView1.GroupSummary.AddRange(New DevExpress.XtraGrid.GridSummaryItem() {New DevExpress.XtraGrid.GridGroupSummaryItem(DevExpress.Data.SummaryItemType.Sum, "UnitPrice", Me.colUnitPrice, "", "UnitPrice"), New DevExpress.XtraGrid.GridGroupSummaryItem(DevExpress.Data.SummaryItemType.Sum, "UnitsInStock", Me.colUnitsInStock, "", "UnitsInStock"), New DevExpress.XtraGrid.GridGroupSummaryItem(DevExpress.Data.SummaryItemType.Custom, "", Me.colPrice, "", "Price")})
+            Me.gridView1.Name = "gridView1"
+            Me.gridView1.OptionsView.ShowFooter = True
+            Me.gridView1.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() {New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.colCategory, DevExpress.Data.ColumnSortOrder.Ascending)})
+            Me.colProductName.Caption = "Product Name"
+            Me.colProductName.FieldName = "ProductName"
+            Me.colProductName.Name = "colProductName"
+            Me.colProductName.Visible = True
+            Me.colProductName.VisibleIndex = 0
+            Me.colUnitsInStock.Caption = "Units In Stock"
+            Me.colUnitsInStock.FieldName = "UnitsInStock"
+            Me.colUnitsInStock.Name = "colUnitsInStock"
+            Me.colUnitsInStock.SummaryItem.SummaryType = DevExpress.Data.SummaryItemType.Sum
+            Me.colUnitsInStock.Visible = True
+            Me.colUnitsInStock.VisibleIndex = 1
+            Me.colUnitPrice.Caption = "UnitPrice"
+            Me.colUnitPrice.FieldName = "UnitPrice"
+            Me.colUnitPrice.Name = "colUnitPrice"
+            Me.colUnitPrice.SummaryItem.SummaryType = DevExpress.Data.SummaryItemType.Sum
+            Me.colUnitPrice.Visible = True
+            Me.colUnitPrice.VisibleIndex = 2
+            Me.colPrice.Caption = "Price"
+            Me.colPrice.FieldName = "Price"
+            Me.colPrice.Name = "colPrice"
+            Me.colPrice.SummaryItem.SummaryType = DevExpress.Data.SummaryItemType.Custom
+            Me.colPrice.UnboundType = DevExpress.Data.UnboundColumnType.Decimal
+            Me.colPrice.Visible = True
+            Me.colPrice.VisibleIndex = 3
+            Me.colCategory.Caption = "Category"
+            Me.colCategory.FieldName = "Category"
+            Me.colCategory.Name = "colCategory"
+            Me.colCategory.Visible = True
+            Me.colCategory.VisibleIndex = 4
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0F, 13.0F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(386, 538)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.productsBindingSource), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		End Sub
-
-		#End Region
-
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private productsBindingSource As System.Windows.Forms.BindingSource
-		Private colProductName As DevExpress.XtraGrid.Columns.GridColumn
-		Private colUnitsInStock As DevExpress.XtraGrid.Columns.GridColumn
-		Private colUnitPrice As DevExpress.XtraGrid.Columns.GridColumn
-		Private colPrice As DevExpress.XtraGrid.Columns.GridColumn
-		Private colCategory As DevExpress.XtraGrid.Columns.GridColumn
-	End Class
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+        Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private productsBindingSource As System.Windows.Forms.BindingSource
+        Private colProductName As DevExpress.XtraGrid.Columns.GridColumn
+        Private colUnitsInStock As DevExpress.XtraGrid.Columns.GridColumn
+        Private colUnitPrice As DevExpress.XtraGrid.Columns.GridColumn
+        Private colPrice As DevExpress.XtraGrid.Columns.GridColumn
+        Private colCategory As DevExpress.XtraGrid.Columns.GridColumn
+    End Class
 End Namespace
-
--- VB/Q273845/Form1.vb
+++ VB/Q273845/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports DevExpress.XtraGrid
+Imports DevExpress.XtraGrid
 Imports DevExpress.XtraGrid.Views.Base
 Imports DevExpress.XtraGrid.Views.Grid
 Imports System
@@ -6,52 +6,48 @@ Imports System.Data
 Imports System.Windows.Forms
 
 Namespace Q273845
-	Partial Public Class Form1
-		Inherits Form
+    Partial Public Class Form1
+        Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-			productsBindingSource.DataSource = GetProductsDataTable()
-		End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            productsBindingSource.DataSource = GetProductsDataTable()
+        End Sub
 
-		Private Function GetProductsDataTable() As DataTable
-			Dim table As New DataTable()
-			table.TableName = "Products"
-			table.Columns.Add(New DataColumn("ProductName", GetType(String)))
-			table.Columns.Add(New DataColumn("UnitsInStock", GetType(Integer)))
-			table.Columns.Add(New DataColumn("UnitPrice", GetType(Double)))
-			table.Columns.Add(New DataColumn("Category", GetType(String)))
-			Dim random As New Random()
-			For i As Integer = 0 To 19
-				Dim index As Integer = i + 1
-				table.Rows.Add("Product " & index, random.Next(1, 20), Math.Round(random.NextDouble() * 1000, 3), "Category " & i Mod 7)
-			Next i
-			Return table
-		End Function
+        Private Function GetProductsDataTable() As DataTable
+            Dim table As DataTable = New DataTable()
+            table.TableName = "Products"
+            table.Columns.Add(New DataColumn("ProductName", GetType(String)))
+            table.Columns.Add(New DataColumn("UnitsInStock", GetType(Integer)))
+            table.Columns.Add(New DataColumn("UnitPrice", GetType(Double)))
+            table.Columns.Add(New DataColumn("Category", GetType(String)))
+            Dim random As Random = New Random()
+            For i As Integer = 0 To 20 - 1
+                Dim index As Integer = i + 1
+                table.Rows.Add("Product " & index, random.[Next](1, 20), Math.Round(random.NextDouble() * 1000, 3), "Category " & i Mod 7)
+            Next
+            Return table
+        End Function
 
-		Private Sub gridView1_CustomDrawFooterCell(ByVal sender As Object, ByVal e As FooterCellCustomDrawEventArgs) Handles gridView1.CustomDrawFooterCell
-			If e.Column Is colPrice Then
-				e.Info.DisplayText = (Convert.ToDecimal(colUnitPrice.SummaryItem.SummaryValue) * Convert.ToDecimal(colUnitsInStock.SummaryItem.SummaryValue)).ToString()
-			End If
-		End Sub
+        Private Sub gridView1_CustomDrawFooterCell(ByVal sender As Object, ByVal e As FooterCellCustomDrawEventArgs) Handles gridView1.CustomDrawFooterCell
+            If e.Column Is colPrice Then e.Info.DisplayText = (Convert.ToDecimal(colUnitPrice.SummaryItem.SummaryValue) * Convert.ToDecimal(colUnitsInStock.SummaryItem.SummaryValue)).ToString()
+        End Sub
 
-		Private Sub gridView1_CustomDrawRowFooterCell(ByVal sender As Object, ByVal e As FooterCellCustomDrawEventArgs) Handles gridView1.CustomDrawRowFooterCell
-			If e.Column Is colPrice Then
-				Dim view As GridView = DirectCast(sender, GridView)
-				e.Info.DisplayText = (Convert.ToDecimal(view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary("UnitPrice"), GridGroupSummaryItem))) * Convert.ToDecimal(view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary("UnitsInStock"), GridGroupSummaryItem)))).ToString()
-			End If
-		End Sub
+        Private Sub gridView1_CustomDrawRowFooterCell(ByVal sender As Object, ByVal e As FooterCellCustomDrawEventArgs) Handles gridView1.CustomDrawRowFooterCell
+            If e.Column Is colPrice Then
+                Dim view As GridView = CType(sender, GridView)
+                e.Info.DisplayText = (Convert.ToDecimal(view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary("UnitPrice"), GridGroupSummaryItem))) * Convert.ToDecimal(view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary("UnitsInStock"), GridGroupSummaryItem)))).ToString()
+            End If
+        End Sub
 
-		Private Sub gridView1_CustomUnboundColumnData(ByVal sender As Object, ByVal e As CustomColumnDataEventArgs) Handles gridView1.CustomUnboundColumnData
-			Dim view As GridView = DirectCast(sender, GridView)
-			If e.Column Is colPrice Then
-				If e.IsGetData Then
-					e.Value = Convert.ToDecimal(view.GetListSourceRowCellValue(e.ListSourceRowIndex, colUnitPrice)) * Convert.ToInt32(view.GetListSourceRowCellValue(e.ListSourceRowIndex, colUnitsInStock))
-				End If
-			End If
-		End Sub
-	End Class
+        Private Sub gridView1_CustomUnboundColumnData(ByVal sender As Object, ByVal e As CustomColumnDataEventArgs) Handles gridView1.CustomUnboundColumnData
+            Dim view As GridView = CType(sender, GridView)
+            If e.Column Is colPrice Then
+                If e.IsGetData Then e.Value = Convert.ToDecimal(view.GetListSourceRowCellValue(e.ListSourceRowIndex, colUnitPrice)) * Convert.ToInt32(view.GetListSourceRowCellValue(e.ListSourceRowIndex, colUnitsInStock))
+            End If
+        End Sub
+    End Class
 End Namespace
